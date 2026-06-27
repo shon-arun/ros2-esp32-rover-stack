2fb4ea58b59613a655d5681d2bccf13930becc92
Shon Arun <shornarun3785@gmail.com>
2026-06-27 21:23:55 +0530

    Update for vSLAM

---
+++ microros_ws/src/rover_bringup/launch/vslam.launch.py
@@ -0,0 +1,54 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    vslam_config = os.path.join(
+        get_package_share_directory('rover_bringup'),
+        'config',
+        'vslam.yaml'
+    )
+
+    return LaunchDescription([
+        # Decompress the edge stream from the ESP32 network
+        Node(
+            package='image_transport',
+            executable='republish',
+            name='republish_compressed',
+            arguments=['compressed', 'raw'],
+            remappings=[
+                ('in/compressed', '/camera/image_raw/compressed'),
+                ('out', '/camera/image_raw_decompressed')
+            ]
+        ),
+
+        # SLAM Node
+        Node(
+            package='rtabmap_slam',
+            executable='rtabmap',
+            name='rtabmap',
+            parameters=[vslam_config],
+            remappings=[
+                ('rgb/image', '/camera/image_raw_decompressed'),
+                ('rgb/camera_info', '/camera/camera_info'),
+                ('odom', '/odometry/filtered')
+            ],
+            arguments=['-d'], # Start with a clean map database
+            output='screen'
+        ),
+        
+        # Visualizer
+        Node(
+            package='rtabmap_viz',
+            executable='rtabmap_viz',
+            name='rtabmap_viz',
+            parameters=[vslam_config],
+            remappings=[
+                ('rgb/image', '/camera/image_raw_decompressed'),
+                ('rgb/camera_info', '/camera/camera_info'),
+                ('odom', '/odometry/filtered')
+            ],
+            output='screen'
+        )
+    ])
