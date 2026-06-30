3f8d158cc91c8c4c905426ae4a3298a0d1b88774
Shon Arun <shonarun3785@gmail.com>
2026-06-30 18:28:40 +0530

    Update start_rover.sh

--- microros_ws/src/rover_bringup/scripts/start_rover.sh
+++ microros_ws/src/rover_bringup/scripts/start_rover.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/ros/jazzy/setup.bash
-source /home/pi/microros_ws/install/setup.bash
+source /home/user/ros2-esp32-rover-stack/microros_ws/install/setup.bash
 ros2 launch rover_bringup rover.launch.py
